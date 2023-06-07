d16f1f1ebaed087ae266303dcd2b4995607f069e
vaniffatiy <mytsykov73@mail.ru>
2023-06-07 15:20:19 +0300

    Initial commit

--- bd-connection-psychopg2-WORKING.py
+++ bd-connection-psychopg2-WORKING.py
@@ -13,9 +13,9 @@ conn = psycopg2.connect(
 
 cur = conn.cursor()
 
-cur.execute("CREATE TABLE Fruit_Basket (id serial PRIMARY KEY, data varchar, num integer);")
-cur.execute("INSERT INTO Fruit_Basket (data, num) VALUES (%s, %s)", ("Banana", 220))
-cur.execute("INSERT INTO Fruit_Basket (data, num) VALUES (%s, %s)", ("Apple", 400))
+# cur.execute("CREATE TABLE Fruit_Basket (id serial PRIMARY KEY, data varchar, num integer);")
+# cur.execute("INSERT INTO Fruit_Basket (data, num) VALUES (%s, %s)", ("Banana", 220))
+# cur.execute("INSERT INTO Fruit_Basket (data, num) VALUES (%s, %s)", ("Apple", 400))
 # cur.execute("DELETE FROM Fruit_Basket WHERE data = 'Apple' AND num = 400")
 # cur.execute("DELETE FROM Fruit_Basket WHERE data = 'Banana' AND num = 220")
 
